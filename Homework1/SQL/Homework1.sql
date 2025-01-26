cf2c004d86cd4c917cbc7ff0d12225d68995d639
Sarah <sarahrosegallagher@gmail.com>
2025-01-26 13:06:34 -0800

    init homework files

---
+++ Homework1/SQL/Homework1.sql
@@ -0,0 +1,17 @@
+-- Question 3: Trip Segmentation Count 
+
+-- Trips <= 1 mile
+
+-- Trips > 1, <= 3
+
+-- Trips > 3, <= 7
+
+-- Trips > 7, <= 10
+
+-- Trips > 10
+
+
+--  Question 4: Day with longest trip distance 
+
+-- Question 5: Three biggest pickup zones 
+
